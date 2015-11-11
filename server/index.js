619e7339bd3f4178218d08d270361e61f674c7c0
spawnius <spawnenius@gmail.com>
2015-11-12 01:45:05 +0200

    Added a self signed cert and started using SSL

--- server/index.js
+++ server/index.js
@@ -1,18 +1,25 @@
-var express = require('express'),
-	app = express(),
-	PeerServer = require('peer').PeerServer,
-	port = process.env.PORT || 3000,
-	path = require('path'),
-	clientFolder = path.resolve(__dirname + '/../client'),
-	rooms = require('./modules/rooms');
+var fs = require('fs'),
+    https = require('https'),
+    express = require('express'),
+    app = express(),
+    PeerServer = require('peer').PeerServer,
+    port = process.env.PORT || 3000,
+    path = require('path'),
+    serverPath = path.resolve(__dirname),
+    rooms = require('./modules/rooms');
 
-var server = app.listen(port),
-	peerRooms = rooms();
+var options = {
+    key: fs.readFileSync(process.env.SSL_KEY_PATH || serverPath +'/ssl/webrtc.key'),
+    cert: fs.readFileSync(process.env.SSl_CRT_PATH || serverPath + '/ssl/webrtc.crt')
+};
+
+var peerRooms = rooms();
+https.createServer(options, app).listen(port);
 
 // Setting up express
-app.set('views', __dirname + '/views');
+app.set('views', serverPath + '/views');
 app.set('view engine', 'jade');
-app.use(express.static(clientFolder));
+app.use(express.static(serverPath + '/../client'));
 
 console.log('Listening on port', port);
 
@@ -22,22 +29,26 @@ app.get('/', function (req, res) {
 });
 
 app.get('/[\\w]{16}', function (req, res) {
-    var roomId = req.path.substring(1);
+  var roomId = req.path.substring(1);
 	res.render('index');
 });
 
 app.get('/api/room/:id', function (req, res) {
-    res.json({
-        peers: peerRooms.getPeers(req.params.id)
-    });
-})
+  res.json({
+    peers: peerRooms.getPeers(req.params.id)
+  });
+});
 
 //PeerJS stuff
-var peerServer = PeerServer({port: 9000, path: '/connect', proxied: true});
+var peerServer = PeerServer({
+    port: 9000, path: '/connect', 
+    proxied: true,
+    ssl: options
+  });
 
 peerServer.on('connection', function (id) {
   var peerId = id.slice(16),
-  	  roomId = id.slice(0, 16);
+      roomId = id.slice(0, 16);
 
   peerRooms.add(roomId, peerId);
   console.log('User connected with #', id);
