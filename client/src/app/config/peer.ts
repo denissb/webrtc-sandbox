7b70e2fa622d271dee00e80044bfdd2c7d14ad5e
spawnius <spawnenius@gmail.com>
2015-11-29 15:27:12 +0200

    Added a list of free STUN and TURN servers to config

--- client/src/app/config/peer.ts
+++ client/src/app/config/peer.ts
@@ -2,4 +2,18 @@ export class PeerConfig {
     static host = '/';
     static port = 9000;
     static path = '/connect';
+    static iceServers = [
+        { url: 'stun:stun.l.google.com:19302' },
+        { url: 'stun:stun1.l.google.com:19302' },
+        { url: 'stun: stun2.l.google.com:19302' },
+        { 
+            url: 'turn:homeo@turn.bistri.com:80', 
+            credential: 'homeo' 
+        },
+        { 
+            url: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com'
+        }
+    ];
 };
